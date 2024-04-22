c88076fb3aebdc99fc0742e5acf131aaeaf95b02
prekshapatil <patil.pre@northeastern.edu>
2024-04-22 13:55:00 -0400

    sta

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,12 +1,12 @@
 import ModuleList from "../Modules/List";
-import Status from "./Status";
+// import Status from "./Status";
 function Home() {
   return (
     <div>
       
       <div className="d-flex justify-content-between">
         <ModuleList />
-        <Status />
+        {/* <Status /> */}
       </div>
     </div>
   );
