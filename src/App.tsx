0cb65f65a77c1914ee8e8179d6601364b9f7a689
prekshapatil <patil.pre@northeastern.edu>
2024-04-22 22:48:41 -0400

    changes to lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
-// import './App.css';
-// import Labs from "./Labs";
-// import HelloWorld from "./Labs/a3/HelloWorld";
+import './App.css';
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
